3291394148a06c09e8600041e273ca752a6af401
William Zhang <zhangjifan5683@gmail.com>
2014-04-16 10:39:25 -0400

    dice.py

--- dice.py
+++ dice.py
@@ -2,4 +2,6 @@ from specialInput import *
 #! /usr/bin/env python
 import random
 sides=int_input("How many sides the die should have?")
-print random.randint(1,sides)
+dice=int_input("How many dice?")
+for roll in range(1,dice):
+    print random.randint(1,sides)
